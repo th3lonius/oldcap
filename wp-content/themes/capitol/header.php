076a2fd28b51bc4cbcd6d3b2a87d041c8f124739
th3lonius <mesaros.creative@gmail.com>
2017-09-12 13:44:20 -0500

    favicons

--- wp-content/themes/capitol/header.php
+++ wp-content/themes/capitol/header.php
@@ -13,11 +13,20 @@
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, user-scalable=no">
-    <title><?php wp_title( '|', true, 'right' ); ?>Old Capitol</title>
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
-    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png?v=5Ae6nvablg">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png?v=5Ae6nvablg">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?php echo get_template_directory_uri(); ?>/android-chrome-192x192.png?v=5Ae6nvablg">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png?v=5Ae6nvablg">
+    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/manifest.json?v=5Ae6nvablg">
+    <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/safari-pinned-tab.svg?v=5Ae6nvablg" color="#5bbad5">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico?v=5Ae6nvablg">
+    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/mstile-144x144.png?v=5Ae6nvablg">
+    <meta name="theme-color" content="#ffffff">
     <?php wp_head(); ?>
     <script src="https://use.typekit.net/fae8swm.js"></script>
     <script>try{Typekit.load({ async: true });}catch(e){}</script>
@@ -25,16 +34,8 @@
     <script src="http://prod3.agileticketing.net/websales/agile_widget.ashx?orgid=2553&epgid=292&" type="text/javascript"></script>
   </head>
 
-<?php if ( is_page( 'about' )) { ?>
-
-  <body class="bg-grey">
-
-<?php } else { ?>
-
   <body>
    
-   <?php } ?>
-
     <header class="global-header">
       
       <button type="button" role="button" class="menu-toggle toggle-push-right"><span class="lines"></span></button>
