31a454fe9a561758d4ffec8a50f5110262b9c20c
th3lonius <mesaros.creative@gmail.com>
2017-09-17 16:16:25 -0500

    Author fix / blog page dev

--- wp-content/themes/capitol/single.php
+++ wp-content/themes/capitol/single.php
@@ -1,49 +1,20 @@
 <?php get_header(); ?>
 
-<main class="interior">
+<main class="blog">
 
   <?php if ( have_posts() ) : ?>
 
     <?php while ( have_posts() ) : the_post(); ?>
-          
-      <?php 
-
-        $image = get_field('image');
-
-        if( !empty($image) ): 
-
-        // vars
-        $url = $image['url'];
-        $title = $image['title'];
-        $alt = $image['alt'];
-        $caption = $image['caption'];
-
-        // thumbnail
-        $size = 'full';
-        $thumb = $image['sizes'][ $size ];
-        $width = $image['sizes'][ $size . '-width' ];
-        $height = $image['sizes'][ $size . '-height' ];
-
-        ?>
-           
-    <article class="intro-image" style="background-image: url(<?php echo $url; ?>);">
-
-      <div class="caption">
-        <h1 class="caption-text"><?php the_title(); ?></h1>
-      </div>
-
-    <?php endif; ?>
-            
-  </article>
-           
-  <article>
+                              
+  <article class="col-8-12 no-float">
     
-   <header>
-        <?php previous_post_link( '%link', 'Older' ); ?>
-        <?php next_post_link( '%link', 'Newer' );	?>
-   </header>
+    <header>
+      <author>by <?php the_author(); ?> </author>
+      <date> on <?php the_date(); ?> @ <?php the_time(); ?></date>
+      <h1><?php the_title(); ?></h1>
+    </header>
 
-    <section class="col-6-12 no-float">
+    <section>
 
       <?php the_content(); ?>
 
@@ -59,6 +30,6 @@
 
   <?php endif; ?>
 
-</main><!-- .interior -->
+</main><!-- .blog -->
 
 <?php get_footer(); ?>
