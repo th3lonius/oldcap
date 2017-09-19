5ddb53ff802352398052efd9b99b72074d9a5ffb
th3lonius <mesaros.creative@gmail.com>
2017-09-19 18:35:44 -0500

    New video

--- wp-content/themes/capitol/partials/module-video.php
+++ wp-content/themes/capitol/partials/module-video.php
@@ -1,13 +1,13 @@
-<article class="intro">
+<article id="homepage">
 
   <header>
-    <span>The</span><em>Tastiest</em><span>in</span><em>Tofu</em>
+    <span>The</span><em>Tastiest</em><span>in</span><em>Tofu</em><span>from</span><em>Iowa</em>
   </header>
 
   <video muted autoplay loop playsinline id="bgvid" poster="<?php bloginfo('template_directory'); ?>/img/batch-still.jpg">
-    <source src="<?php bloginfo('template_directory'); ?>/video/oldcap-process-web-2.mp4" type="video/mp4">
-    <source src="<?php bloginfo('template_directory'); ?>/video/oldcap-process-web-2.webm" type="video/webm">
-    <source src="<?php bloginfo('template_directory'); ?>/video/oldcap-process-web.ogv" type="video/ogg">
+    <source src="<?php bloginfo('template_directory'); ?>/video/oldcap-montage-web.mp4" type="video/mp4">
+    <source src="<?php bloginfo('template_directory'); ?>/video/oldcap-montage-web.webm" type="video/webm">
+    <source src="<?php bloginfo('template_directory'); ?>/video/oldcap-montage-web.ogv" type="video/ogg">
   </video>
 
   <!--<a href="#" class="learn-more floating"></a>-->
