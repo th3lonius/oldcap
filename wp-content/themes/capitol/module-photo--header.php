bcd8caa69777d41025f894d7611d3a6b54259e44
th3lonius <mesaros.creative@gmail.com>
2017-10-01 16:45:21 -0500

    Removed randimage query / added jordan avatar

--- wp-content/themes/capitol/module-photo--header.php
+++ wp-content/themes/capitol/module-photo--header.php
@@ -20,22 +20,4 @@
 
   <?php } else {
 
-    $args = array(
-        'post_type' => 'attachment',
-        'post_status' => 'inherit',
-        'tag' => 'fields',
-        'posts_per_page' => 1,
-        'fields' => 'ids',
-        'orderby' => 'rand'
-    );
-
-    $randimage = new WP_Query( $args );
-
-    if( $randimage->have_posts() ){
-        $image_attributes = wp_get_attachment_image_src( $randimage->posts[0], 'large' );
-    ?>
-        <section data-stellar-background-ratio="2" style="background-image: url(<?php echo $image_attributes[0]; ?>);">
-          
-<?php }
-
   } ?>
