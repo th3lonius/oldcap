50d5828f5d9b1027505a6cc54bb16684b230fca4
th3lonius <mesaros.creative@gmail.com>
2017-10-21 10:35:25 -0500

    Blogroll styling

--- wp-content/themes/capitol/front-page.php
+++ wp-content/themes/capitol/front-page.php
@@ -8,4 +8,29 @@
   
 </main>
 
+<button type="button" role="button" class="blog-toggle toggle-push-right">
+  <span>News & Updates</span>
+  <span>Close</span>
+</button>
+
+<aside id="blogroll">
+  
+  <?php if ( have_posts() ) : ?>
+
+    <?php while ( have_posts() ) : the_post(); ?>
+  
+      <ol>
+
+        <li><a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a></li>
+        <li><?php the_date(); ?> @ <?php the_time(); ?></li>
+        <li><?php the_excerpt(); ?></li>
+
+      </ol>
+  
+    <?php endwhile; ?>
+  
+  <?php endif; ?>
+  
+</aside>
+
 <?php get_footer(); ?>
