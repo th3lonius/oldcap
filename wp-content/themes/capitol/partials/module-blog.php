02a26b30c387276cce7adf444f30f7bd7f879640
th3lonius <mesaros.creative@gmail.com>
2017-12-15 10:22:24 -0600

    added frontpage latest-recipes module

--- wp-content/themes/capitol/partials/module-blog.php
+++ wp-content/themes/capitol/partials/module-blog.php
@@ -1,21 +1,27 @@
-  <article class="module--blog">
-    
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    
-    <article>
-    
-      <header>
-        <h1 class="article-title"><?php the_title() ?></h1>
-        <date><?php the_time('F j, Y'); ?></date>
-        <cite>by <?php the_author_firstname(); ?> <?php the_author_lastname(); ?></cite>
-      </header>
-      
-      <section class="content">
-        <?php the_content(); ?>
-      </section>
-      
-    </article>
-        
-    <?php endwhile; endif; ?>
+<section class="module--blogroll col-6-12">
+
+  <?php if ( have_posts() ) : ?>
+  
+  <h3>Blogroll</h3>
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+  <article>
+
+    <header>
+      <h1 class="article-title"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h1>
+      <date><?php the_date(); ?> @ <?php the_time('F j, Y'); ?></date>
+      <cite>by <?php the_author_firstname(); ?> <?php the_author_lastname(); ?></cite>
+    </header>
+
+    <section>
+      <?php the_excerpt(); ?>
+    </section>
 
   </article>
+
+    <?php endwhile; ?>
+  
+  <?php endif; ?>
+
+</section>
