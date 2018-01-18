30493ebc58ac07710794db2dced94de9fe0ec42a
th3lonius <mesaros.creative@gmail.com>
2018-01-17 19:14:58 -0600

    new blogroll style

--- wp-content/themes/capitol/partials/module-blog.php
+++ wp-content/themes/capitol/partials/module-blog.php
@@ -1,4 +1,4 @@
-<section class="blogroll container__col-sm-12 container__col-md-12">
+<section class="blogroll container__row">
   
 <?php
 
@@ -15,17 +15,17 @@
   
   <h3 class="section-title st-blogroll">Blogroll</h3>
   
-      <div class="horizontal-scroll-container">
-
         <?php while ( $query->have_posts() ) : $query->the_post(); ?>
 
-        <article>
+        <article class="container__col-sm-12 container__col-md-6 container__col-lg-3">
 
-          <header>
-            <h4><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h4>
-            <date><?php the_date('F j, Y'); ?> @ <?php the_time('g:ia'); ?></date>
-            <cite>by <?php the_author_firstname(); ?> <?php the_author_lastname(); ?></cite>
-          </header>
+          <figure>
+            <a href="<?php the_permalink(); ?>"><img src="<?php the_field('image'); ?>" /></a>
+          </figure>
+
+          <h4><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h4>
+          <date><?php the_date('F j, Y'); ?> @ <?php the_time('g:ia'); ?></date>
+          <cite>by <?php the_author_firstname(); ?> <?php the_author_lastname(); ?></cite>
 
           <section class="content-block">
             <?php the_excerpt(); ?>
@@ -34,8 +34,6 @@
         </article>
 
       <?php endwhile; ?>
-        
-      </div>
   
   <?php endif; ?>
 
--- wp-content/themes/capitol/partials/module-our-story.php
+++ wp-content/themes/capitol/partials/module-our-story.php
@@ -1,7 +1,8 @@
-<section id="module-our-story" class="container__col-sm-12 container__col-md-8">
+<section id="module-our-story" class="container__col-sm-12">
   
   <figure>
-    <div>
+    <div class="bg-image"></div>
+    <div class="module-textblock">
       <h2 class="section-title">Our Story</h2>
       <p>Nulla volutpat aliquet eros eget vulputate. Integer varius ultrices purus a mattis. Aliquam magna enim, semper pharetra tempor sed, pellentesque a dui. Etiam in tempus lorem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus facilisis volutpat libero, non semper justo pulvinar sit amet. Phasellus accumsan luctus erat, ut volutpat diam efficitur sed. Pellentesque tincidunt dui dolor, pretium pharetra lacus suscipit vel. Duis lobortis mollis lectus, non maximus arcu venenatis id.</p>
     </div>
