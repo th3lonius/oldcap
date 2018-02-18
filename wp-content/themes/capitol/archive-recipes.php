e5212c65ec93f9274312c6426adc9b58adc5b189
th3lonius <mesaros.creative@gmail.com>
2018-02-18 12:11:38 -0600

    updated from master

--- wp-content/themes/capitol/archive-recipes.php
+++ wp-content/themes/capitol/archive-recipes.php
@@ -22,34 +22,6 @@ get_header();
 <?php if ( $query->have_posts() ) : ?>
 
   <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-      
-    <?php include(locate_template('module-photo--header.php')); ?>
-
-      <header class="intro-details">
-        
-        <span class="serving">Serving <?php the_field('servings'); ?></span>
-
-        <a href="<?php the_permalink(); ?>" class="block">
-          <h1 class="intro-title recipe-title"><?php the_title(); ?></h1>
-        </a>
-        
-        <?php $authors = get_field('author');
-
-        if( $authors ): ?>
-
-        <?php foreach( $authors as $author ): // variable must NOT be called $post (IMPORTANT) ?>
-
-        <author>by <a href="<?php echo get_permalink( $author->ID ); ?>"><?php echo get_the_title( $author->ID ); ?></a></author>
-
-        <?php endforeach; ?>
-
-        <?php endif; ?>
-
-        <?php the_excerpt(); ?>
-
-      </header>
-
-    </section>
   
     <?php get_template_part( 'module', 'recipe--intro' ); ?>   
 
